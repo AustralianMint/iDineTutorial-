957d06292c880786f24d5646246f6a058fe6c60f
AustralianMint <thomas.frey@code.berlin>
2023-03-29 16:59:07 +0200

    add project

---
+++ iDinePractice/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  iDinePractice
+//
+//  Created by Thomas Frey on 29.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //save info from json in a constant
+    let thisIsGreatMenu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                //Nested ForEach Loop filling struct w/ json data
+                ForEach(thisIsGreatMenu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Json List")
+            .listStyle(.grouped)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDinePractice/iDinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDinePracticeApp.swift
+//  iDinePractice
+//
+//  Created by Thomas Frey on 29.03.23.
+//
+
+import SwiftUI
+
+@main
+struct iDinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
